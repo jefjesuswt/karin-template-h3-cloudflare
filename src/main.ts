44cdbe26d6ac257168068845947b93c166e49bf7
Jeffrey Jesus <jefjesuswt@gmail.com>
2025-12-04 14:03:39 -0400

    chore: update template

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { KarinFactory } from "@karin-js/core";
-import { H3Adapter } from "@karin-js/platform-h3";
+import { KarinFactory } from "@project-karin/core";
+import { H3Adapter } from "@project-karin/platform-h3";
 import { AppController } from "./app/app.controller";
 
 const app = await KarinFactory.create(new H3Adapter(), {
